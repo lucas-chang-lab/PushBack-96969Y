3bcebf8ca7b175bb22b95870b720fe95bbdac5cf
Dancingcowz <rexyblan@gmail.com>
2025-08-15 14:07:58 +0800

    intake ez, without other scenario

---
+++ include/Mechanics/botIntake.h
@@ -0,0 +1,19 @@
+#pragma once
+
+
+namespace botIntake {
+    void runThread();
+
+    void preauton();
+
+    void setState(int, double = 0);
+
+    void switchState();
+
+    void control(int);
+
+    bool canControl();
+    
+    extern int _taskState;
+    extern double _taskDelay;
+}
