7456fb17220926a36a3939e7140214e43239a042
Dancingcowz <rexyblan@gmail.com>
2025-08-15 14:09:47 +0800

    robot-config

--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -14,6 +14,6 @@ motor RightMotorC(PORT19, ratio6_1, true);
 motor_group LeftMotors(LeftMotorA, LeftMotorB, LeftMotorC);
 motor_group RightMotors(RightMotorA, RightMotorB, RightMotorC);
 motor_group LeftRightMotors(LeftMotorA, LeftMotorB, LeftMotorC, RightMotorA, RightMotorB, RightMotorC);
-
+motor IntakeMotor(PORT10, ratio18_1, false);
 
 inertial InertialSensor(PORT2); 
